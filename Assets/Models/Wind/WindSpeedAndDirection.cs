c3b31c4442cc3e75b90c977366f0fcffb1c4efdd
Dominykas Sipelis <dsipel2@uic.edu>
2020-09-14 18:09:08 -0500

    implemented temperature and wind objects

---
+++ Assets/Models/Wind/WindSpeedAndDirection.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+
+public class WindSpeedAndDirection : MonoBehaviour {
+    public GameObject windSock;
+    public GameObject pivot;
+    public GameObject windText;
+    string url = "http://api.openweathermap.org/data/2.5/weather?lat=41.88&lon=-87.6&APPID=285c4fa1813be8efc39220903a9af06f&units=imperial";
+
+
+    void Start() {
+        // wait a couple seconds to start and then refresh every 900 seconds
+
+        InvokeRepeating("GetDataFromWeb", 2f, 900f);
+    }
+
+    void GetDataFromWeb() {
+        StartCoroutine(GetRequest(url));
+    }
+
+    IEnumerator GetRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError) {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else {
+                string json = webRequest.downloadHandler.text;
+                // print out the weather data to make sure it makes sense
+                Debug.Log(":\nReceived: " + json);
+
+                //parse wind speed and direction from json
+                int direction = int.Parse(json.Substring(json.IndexOf("deg") + 5, 2));
+                float speed = float.Parse(json.Substring(json.IndexOf("speed") + 7, 4));
+
+                //set text of wind speed
+                windText.GetComponent<TextMeshPro>().text = speed + " MPH";
+
+                //get accurate direction and scale of wind sock
+                direction = 360 - direction;
+                float scaleY = (float)1.5 / (speed / 10);
+
+                //set direction and scale of wind sock
+                pivot.transform.Rotate(new Vector3(0, direction, 0));
+                windSock.transform.localScale = new Vector3(1, scaleY, 1);
+            }
+        }
+    }
+}
---
+++ Assets/Models/thermometer/WeatherAPIScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+
+public class WeatherAPIScript : MonoBehaviour {
+    public GameObject temperature;
+    public GameObject humidity;
+    public GameObject thermometer;
+    string url = "http://api.openweathermap.org/data/2.5/weather?lat=41.88&lon=-87.6&APPID=285c4fa1813be8efc39220903a9af06f&units=imperial";
+
+
+    void Start() {
+
+        // wait a couple seconds to start and then refresh every 900 seconds
+
+        InvokeRepeating("GetDataFromWeb", 2f, 900f);
+    }
+
+    void GetDataFromWeb() {
+
+        StartCoroutine(GetRequest(url));
+    }
+
+    IEnumerator GetRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError) {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else {
+                string json = webRequest.downloadHandler.text;
+                // print out the weather data to make sure it makes sense
+                Debug.Log(":\nReceived: " + json);
+                
+                //parse temperature and humidity from json
+                int temp = int.Parse(json.Substring(json.IndexOf("temp") + 6, 2));
+                int hum = int.Parse(json.Substring(json.IndexOf("humidity") + 10, 2));
+
+                //set temperuture and humidity text
+                temperature.GetComponent<TextMeshPro>().text = temp + " F";
+                humidity.GetComponent<TextMeshPro>().text = hum + "%";
+
+                //change scale of thermometer based on temperature
+                float scaleY = (float)15.75 * (float)(temp / 100.0);
+                thermometer.transform.localScale = new Vector3(5, scaleY, 150);
+            }
+        }
+    }
+}
